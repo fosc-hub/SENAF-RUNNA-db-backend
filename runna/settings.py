5dced340570ecdbdd6134d8016a2479b131e68f1
facuolivamar <facundoolivam@gmail.com>
2024-10-31 14:08:13 -0300

    POSTGRES DB

--- runna/settings.py
+++ runna/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 import os
+import dj_database_url
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -108,10 +112,15 @@ WSGI_APPLICATION = 'runna.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':dj_database_url.config(default=os.getenv('DATABASE_URL')),
+    # 'default': {
+    #     'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.sqlite3'),
+    #     'NAME':  os.getenv('DB_NAME', default=BASE_DIR / 'db.sqlite3'),
+    #     'USER': os.getenv('DB_USER', default=''),
+    #     'PASSWORD': os.getenv('DB_PASSWORD', default=''),
+    #     'HOST': os.getenv('DB_HOST', default='localhost'),
+    #     'PORT': os.getenv('DB_PORT', default='5432'),
+    # }
 }
 
 
