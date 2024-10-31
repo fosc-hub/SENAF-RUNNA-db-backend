05e5a28e3803be12c31f8a63c3828809692cf926
facuolivamar <facundoolivam@gmail.com>
2024-10-31 00:35:59 -0300

    requirements + setting + Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn runna.wsgi
--- runna/runna/settings.py
+++ runna/runna/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,6 +76,11 @@ MIDDLEWARE = [
 
     # corsheaders middleware    
     "corsheaders.middleware.CorsMiddleware",
+
+    # ...
+    # "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    # ...
 ]
 
 ROOT_URLCONF = 'runna.urls'
@@ -144,6 +150,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
