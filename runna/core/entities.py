eb73548c7064977f9417292a7aba6e7c9dbe2251
facuolivamar <facundoolivam@gmail.com>
2024-11-13 00:48:55 -0300

    Core modules (django-independent): entities and use cases

--- runna/core/entities.py
+++ runna/core/entities.py
@@ -1,4 +1,6 @@
 '''
+entity example: 
+
 class Product:
     def __init__(self, name: str, description: str, price: float):
         self.name = name
@@ -10,3 +12,95 @@ class Product:
         self.price -= self.price * (percentage / 100)
 
 '''
+
+class Localizacion:
+    def __init__(self, calle: str, numero: int, referencia_geo: str, barrio):
+        self.calle = calle
+        self.numero = numero
+        self.referencia_geo = referencia_geo
+        self.barrio = barrio
+
+class UsuarioLinea:
+    def __init__(self, nombre: str, apellido: str, fecha_nacimiento, sexo: str, telefono: str, vinculo=None, institucion=None, responsable=None):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.fecha_nacimiento = fecha_nacimiento
+        self.sexo = sexo
+        self.telefono = telefono
+        self.vinculo = vinculo
+        self.institucion = institucion
+        self.responsable = responsable
+
+class Demanda:
+    def __init__(self, fecha_ingreso, hora_ingreso, descripcion: str, ultima_actualizacion, score: int, score_vulneracion: int, score_evaluacion: int, localizacion, usuario_linea):
+        self.fecha_ingreso = fecha_ingreso
+        self.hora_ingreso = hora_ingreso
+        self.descripcion = descripcion
+        self.ultima_actualizacion = ultima_actualizacion
+        self.score = score
+        self.score_vulneracion = score_vulneracion
+        self.score_evaluacion = score_evaluacion
+        self.localizacion = localizacion
+        self.usuario_linea = usuario_linea
+
+class PrecalificacionDemanda:
+    def __init__(self, fecha, hora, descripcion: str, estado_demanda: str, demanda):
+        self.fecha = fecha
+        self.hora = hora
+        self.descripcion = descripcion
+        self.estado_demanda = estado_demanda
+        self.demanda = demanda
+
+class Persona:
+    def __init__(self, nombre: str, apellido: str, fecha_nacimiento, sexo: str, observaciones=None, adulto: bool = False):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.fecha_nacimiento = fecha_nacimiento
+        self.sexo = sexo
+        self.observaciones = observaciones
+        self.adulto = adulto
+
+class DemandaPersona:
+    def __init__(self, demanda, persona, conviviente: bool, autordv: bool, autordv_principal: bool, nnya: bool, nnya_principal: bool):
+        self.demanda = demanda
+        self.persona = persona
+        self.conviviente = conviviente
+        self.autordv = autordv
+        self.autordv_principal = autordv_principal
+        self.nnya = nnya
+        self.nnya_principal = nnya_principal
+
+class NNyA:
+    def __init__(self, persona, educacion=None, institucion_sanitaria=None):
+        self.persona = persona
+        self.educacion = educacion
+        self.institucion_sanitaria = institucion_sanitaria
+
+class NNyAEducacion:
+    def __init__(self, curso: str, nivel: str, turno: str, comentarios: str, institucion_educativa=None):
+        self.curso = curso
+        self.nivel = nivel
+        self.turno = turno
+        self.comentarios = comentarios
+        self.institucion_educativa = institucion_educativa
+
+class Vulneracion:
+    def __init__(self, vulneracion_principal_demanda: bool, sumatoria_pesos: int, demanda, persona_nnya, persona_autordv=None, categoria_motivo=None, categoria_submotivo=None, gravedad=None, urgencia=None):
+        self.vulneracion_principal_demanda = vulneracion_principal_demanda
+        self.sumatoria_pesos = sumatoria_pesos
+        self.demanda = demanda
+        self.persona_nnya = persona_nnya
+        self.persona_autordv = persona_autordv
+        self.categoria_motivo = categoria_motivo
+        self.categoria_submotivo = categoria_submotivo
+        self.gravedad = gravedad
+        self.urgencia = urgencia
+
+class Decision:
+    def __init__(self, fecha, hora, decision: str, justificacion: str, demanda):
+        self.fecha = fecha
+        self.hora = hora
+        self.decision = decision
+        self.justificacion = justificacion
+        self.demanda = demanda
+
--- runna/core/use_cases.py
+++ runna/core/use_cases.py
@@ -1,4 +1,6 @@
 '''
+use case example
+
 from core.entities import Product
 
 class ProductUseCase:
@@ -12,3 +14,64 @@ class ProductUseCase:
         return product
 
 '''
+
+from core.entities import Decision, Demanda, DemandaPersona, NNyA, Persona, Localizacion, PrecalificacionDemanda, UsuarioLinea, Vulneracion
+
+class LocalizacionUseCase:
+    def create_localizacion(self, calle, numero, referencia_geo, barrio):
+        """Creates a new Localizacion entity."""
+        return Localizacion(calle, numero, referencia_geo, barrio)
+
+class UsuarioLineaUseCase:
+    def create_usuario_linea(self, nombre, apellido, fecha_nacimiento, sexo, telefono, vinculo=None, institucion=None, responsable=None):
+        """Creates a new UsuarioLinea entity."""
+        return UsuarioLinea(nombre, apellido, fecha_nacimiento, sexo, telefono, vinculo, institucion, responsable)
+
+class DemandaUseCase:
+    def create_demanda(self, fecha_ingreso, hora_ingreso, descripcion, ultima_actualizacion, score, score_vulneracion, score_evaluacion, localizacion, usuario_linea):
+        """Creates a new Demanda entity."""
+        return Demanda(fecha_ingreso, hora_ingreso, descripcion, ultima_actualizacion, score, score_vulneracion, score_evaluacion, localizacion, usuario_linea)
+
+    def update_score(self, demanda, new_score):
+        """Updates the score of the Demanda."""
+        demanda.score = new_score
+        return demanda
+
+class PrecalificacionDemandaUseCase:
+    def create_precalificacion_demanda(self, fecha, hora, descripcion, estado_demanda, demanda):
+        """Creates a new PrecalificacionDemanda entity."""
+        return PrecalificacionDemanda(fecha, hora, descripcion, estado_demanda, demanda)
+
+class PersonaUseCase:
+    def create_persona(self, nombre, apellido, fecha_nacimiento, sexo, observaciones=None, adulto=False):
+        """Creates a new Persona entity."""
+        return Persona(nombre, apellido, fecha_nacimiento, sexo, observaciones, adulto)
+
+    def update_persona(self, persona, observaciones):
+        """Updates the observaciones of the persona."""
+        persona.observaciones = observaciones
+        return persona
+
+class DemandaPersonaUseCase:
+    def link_persona_to_demanda(self, demanda, persona, conviviente, autordv, autordv_principal, nnya, nnya_principal):
+        """Links a Persona to a Demanda."""
+        return DemandaPersona(demanda, persona, conviviente, autordv, autordv_principal, nnya, nnya_principal)
+
+class NNyAUseCase:
+    def create_nnya(self, persona, educacion=None, institucion_sanitaria=None):
+        """Creates a new NNyA entity."""
+        return NNyA(persona, educacion, institucion_sanitaria)
+
+class VulneracionUseCase:
+    def create_vulneracion(self, vulneracion_principal_demanda, sumatoria_pesos, demanda, persona_nnya, persona_autordv=None, categoria_motivo=None, categoria_submotivo=None, gravedad=None, urgencia=None):
+        """Creates a new Vulneracion entity."""
+        return Vulneracion(vulneracion_principal_demanda, sumatoria_pesos, demanda, persona_nnya, persona_autordv, categoria_motivo, categoria_submotivo, gravedad, urgencia)
+
+    def calculate_vulneracion_score(self, vulneracion):
+        """Calculates and returns the score of a Vulneracion based on its properties."""
+        return vulneracion.sumatoria_pesos
+
+class DecisionUseCase:
+    def make_decision(self, fecha, hora, decision, justificacion, demanda):
+        """Creates a new Decision entity."""
+        return Decision(fecha, hora, decision, justificacion, demanda)
