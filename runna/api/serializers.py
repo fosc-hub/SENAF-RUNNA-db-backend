0ec7dc273a4bcca20f69005176ef7a11decb8360
facuolivamar <facundoolivam@gmail.com>
2024-11-13 00:52:32 -0300

    Serializers: Should be models-independent, but for faster development, serializers.ModelSerializer is chosen.

--- runna/api/serializers.py
+++ runna/api/serializers.py
@@ -1,8 +1,39 @@
 # api/serializers.py
-from rest_framework import serializers
 '''
 class ProductSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=100)
     description = serializers.CharField()
     price = serializers.DecimalField(max_digits=10, decimal_places=2)
 '''
+
+from rest_framework import serializers
+from infrastructure.models import TLocalizacion, TUsuarioLinea, TDemanda, TPersona, TVulneracion
+
+class LocalizacionModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TLocalizacion
+        fields = '__all__'
+
+
+class UsuarioLineaModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TUsuarioLinea
+        fields = '__all__'
+
+
+class DemandaModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TDemanda
+        fields = '__all__'
+
+
+class PersonaModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TPersona
+        fields = ['nombre', 'apellido', 'fecha_nacimiento', 'sexo', 'observaciones', 'adulto']
+
+
+class VulneracionModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TVulneracion
+        fields = '__all__'
