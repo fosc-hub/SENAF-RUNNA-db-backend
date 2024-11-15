0b6e03fe675f55da696e76b74b8b42fbc61cefe7
facuolivamar <facundoolivam@gmail.com>
2024-11-15 17:01:42 -0300

    entities and use cases

--- runna/core/entities.py
+++ runna/core/entities.py
@@ -1,330 +1,43 @@
-'''
-entity example: 
-
-class Product:
-    def __init__(self, name: str, description: str, price: float):
-        self.name = name
-        self.description = description
-        self.price = price
-
-    def apply_discount(self, percentage: float):
-        """Apply a discount to the product price."""
-        self.price -= self.price * (percentage / 100)
-
-'''
-
-class User:
-    def __init__(self, username: str, email: str, fecha_nacimiento: str = None, genero: str = None, telefono: int = None):
-        self.username = username
-        self.email = email
-        self.fecha_nacimiento = fecha_nacimiento
-        self.genero = genero
-        self.telefono = telefono
-
-    def __str__(self):
-        return f"{self.username} ({self.email})"
 
 class Provincia:
-    def __init__(self, nombre: str):
+    def __init__(self, id: int, nombre: str):
+        self.id = id
         self.nombre = nombre
 
 class Departamento:
-    def __init__(self, nombre: str):
-        self.nombre = nombre
-
-class Localidad:
-    def __init__(self, nombre: str, provincia: Provincia, departamento: Departamento):
+    def __init__(self, id: int, nombre: str, provincia: Provincia):
+        self.id = id
         self.nombre = nombre
         self.provincia = provincia
+
+class Localidad:
+    def __init__(self, id: int,nombre: str, departamento: Departamento):
+        self.id = id
+        self.nombre = nombre
         self.departamento = departamento
 
 class Barrio:
-    def __init__(self, nombre: str, localidad: Localidad):
+    def __init__(self, id: int ,nombre: str, localidad: Localidad):
+        self.id = id
         self.nombre = nombre
         self.localidad = localidad
 
 class CPC:
-    def __init__(self, nombre: str):
+    def __init__(self, id: int ,nombre: str, localidad: Localidad):
+        self.id = id
         self.nombre = nombre
+        self.localidad = localidad
 
 class Localizacion:
-    def __init__(self, calle: str, tipo_calle: str, barrio: Barrio, localidad: Localidad, cpc: CPC = None, piso_depto: int = None, lote: int = None, mza: int = None, casa_nro: int = None, referencia_geo: str = None):
+    def __init__(self, id: int, calle: str, tipo_calle: str, localidad: Localidad, barrio: Barrio = None, cpc: CPC = None, piso_depto: int = None, lote: int = None, mza: int = None, casa_nro: int = None, referencia_geo: str = None):
+        self.id = id
         self.calle = calle
         self.tipo_calle = tipo_calle
-        self.barrio = barrio
         self.localidad = localidad
+        self.barrio = barrio
         self.cpc = cpc
         self.piso_depto = piso_depto
         self.lote = lote
         self.mza = mza
         self.casa_nro = casa_nro
         self.referencia_geo = referencia_geo
-
-class InstitucionUsuarioLinea:
-    def __init__(self, nombre: str, contacto: str):
-        self.nombre = nombre
-        self.contacto = contacto
-
-class VinculoUsuarioLinea:
-    def __init__(self, nombre: str):
-        self.nombre = nombre
-
-class Cargo:
-    def __init__(self, nombre: str):
-        self.nombre = nombre
-
-class Responsable:
-    def __init__(self, nombre: str, apellido: str, cargo: Cargo = None):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.cargo = cargo
-
-class UsuarioLinea:
-    def __init__(self, nombre: str, apellido: str, genero: str, vinculo_usuario_linea: VinculoUsuarioLinea, institucion_usuario_linea: InstitucionUsuarioLinea, responsable: Responsable = None, fecha_nacimiento: str = None, telefono: int = None):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.genero = genero
-        self.vinculo_usuario_linea = vinculo_usuario_linea
-        self.institucion_usuario_linea = institucion_usuario_linea
-        self.responsable = responsable
-        self.fecha_nacimiento = fecha_nacimiento
-        self.telefono = telefono
-
-class Demanda:
-    def __init__(self, fecha_ingreso: str, hora_ingreso: str, origen: str, localizacion: Localizacion, usuario_linea: UsuarioLinea, ultima_actualizacion: str, nro_notificacion_102: int = None, nro_sac: int = None, nro_suac: int = None, nro_historia_clinica: int = None, nro_oficio_web: int = None, descripcion: str = None):
-        self.fecha_ingreso = fecha_ingreso
-        self.hora_ingreso = hora_ingreso
-        self.origen = origen
-        self.localizacion = localizacion
-        self.usuario_linea = usuario_linea
-        self.nro_notificacion_102 = nro_notificacion_102
-        self.nro_sac = nro_sac
-        self.nro_suac = nro_suac
-        self.nro_historia_clinica = nro_historia_clinica
-        self.ultima_actualizacion = ultima_actualizacion
-        self.nro_oficio_web = nro_oficio_web
-        self.descripcion = descripcion
-
-class PrecalificacionDemanda:
-    def __init__(self, fecha: str, hora: str, descripcion: str, estado_demanda: str, demanda: Demanda):
-        self.fecha = fecha
-        self.hora = hora
-        self.descripcion = descripcion
-        self.estado_demanda = estado_demanda
-        self.demanda = demanda
-
-class Persona:
-    def __init__(self, nombre: str, apellido: str, fecha_nacimiento: str, situacion_dni: str, genero: str, edad_aproximada: int = None, dni: int = None, boton_antipanico: bool = False, observaciones: str = None, adulto: bool = False, nnya: bool = False):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.fecha_nacimiento = fecha_nacimiento
-        self.situacion_dni = situacion_dni
-        self.genero = genero
-        self.edad_aproximada = edad_aproximada
-        self.dni = dni
-        self.boton_antipanico = boton_antipanico
-        self.observaciones = observaciones
-        self.adulto = adulto
-        self.nnya = nnya
-
-class DemandaPersona:
-    def __init__(self, demanda: Demanda, persona: Persona, conviviente: bool = False, supuesto_autordv: bool = False, supuesto_autordv_principal: bool = False, nnya: bool = False, nnya_principal: bool = False):
-        self.demanda = demanda
-        self.persona = persona
-        self.conviviente = conviviente
-        self.supuesto_autordv = supuesto_autordv
-        self.supuesto_autordv_principal = supuesto_autordv_principal
-        self.nnya = nnya
-        self.nnya_principal = nnya_principal
-
-class InstitucionEducativa:
-    def __init__(self, nombre: str, mail: str = None, telefono: int = None):
-        self.nombre = nombre
-        self.mail = mail
-        self.telefono = telefono
-
-class NNyAEducacion:
-    def __init__(self, curso: str, nivel: str, turno: str, comentarios: str = None, institucion_educativa: InstitucionEducativa = None):
-        self.curso = curso
-        self.nivel = nivel
-        self.turno = turno
-        self.comentarios = comentarios
-        self.institucion_educativa = institucion_educativa
-
-class InstitucionSanitaria:
-    def __init__(self, nombre: str, mail: str = None, telefono: int = None):
-        self.nombre = nombre
-        self.mail = mail
-        self.telefono = telefono
-
-class NNyA:
-    def __init__(self, persona: Persona, educacion: NNyAEducacion, institucion_sanitaria: InstitucionSanitaria):
-        self.persona = persona
-        self.educacion = educacion
-        self.institucion_sanitaria = institucion_sanitaria
-
-class CategoriaMotivo:
-    def __init__(self, nombre: str, descripcion: str = None, peso: int = 0):
-        self.nombre = nombre
-        self.descripcion = descripcion
-        self.peso = peso
-
-class CategoriaSubmotivo:
-    def __init__(self, nombre: str, descripcion: str = None, peso: int = 0, motivo: CategoriaMotivo = None):
-        self.nombre = nombre
-        self.descripcion = descripcion
-        self.peso = peso
-        self.motivo = motivo
-
-class GravedadVulneracion:
-    def __init__(self, nombre: str, descripcion: str = None, peso: int = 0):
-        self.nombre = nombre
-        self.descripcion = descripcion
-        self.peso = peso
-
-class UrgenciaVulneracion:
-    def __init__(self, nombre: str, descripcion: str = None, peso: int = 0):
-        self.nombre = nombre
-        self.descripcion = descripcion
-        self.peso = peso
-
-class Vulneracion:
-    def __init__(self, principal_demanda: bool, transcurre_actualidad: bool, sumatoria_de_pesos: int, demanda: Demanda, nnya: Persona, autor_dv: Persona = None, categoria_motivo: CategoriaMotivo = None, categoria_submotivo: CategoriaSubmotivo = None, gravedad_vulneracion: GravedadVulneracion = None, urgencia_vulneracion: UrgenciaVulneracion = None):
-        self.principal_demanda = principal_demanda
-        self.transcurre_actualidad = transcurre_actualidad
-        self.sumatoria_de_pesos = sumatoria_de_pesos
-        self.demanda = demanda
-        self.nnya = nnya
-        self.autor_dv = autor_dv
-        self.categoria_motivo = categoria_motivo
-        self.categoria_submotivo = categoria_submotivo
-        self.gravedad_vulneracion = gravedad_vulneracion
-        self.urgencia_vulneracion = urgencia_vulneracion
-
-class Decision:
-    def __init__(self, fecha: str, hora: str, justificacion: str, decision: str, demanda: Demanda):
-        self.fecha = fecha
-        self.hora = hora
-        self.justificacion = justificacion
-        self.decision = decision
-        self.demanda = demanda
-
-class DemandaAsignado:
-    def __init__(self, demanda: Demanda, user: User, esta_activo: bool = True, recibido: bool = False, comentarios: str = None):
-        self.demanda = demanda
-        self.user = user
-        self.esta_activo = esta_activo
-        self.recibido = recibido
-        self.comentarios = comentarios
-
-class ActividadTipo:
-    def __init__(self, nombre: str):
-        self.nombre = nombre
-
-class InstitucionActividad:
-    def __init__(self, nombre: str):
-        self.nombre = nombre
-
-class InstitucionRespuesta:
-    def __init__(self, nombre: str):
-        self.nombre = nombre
-
-class Actividad:
-    def __init__(self, fecha: str, hora: str, descripcion: str, demanda: Demanda, tipo: ActividadTipo = None, institucion: InstitucionActividad = None):
-        self.fecha = fecha
-        self.hora = hora
-        self.descripcion = descripcion
-        self.demanda = demanda
-        self.tipo = tipo
-        self.institucion = institucion
-
-class Respuesta:
-    def __init__(self, fecha: str, hora: str, mail: str, mensaje: str, demanda: Demanda, institucion: InstitucionRespuesta = None):
-        self.fecha = fecha
-        self.hora = hora
-        self.mail = mail
-        self.mensaje = mensaje
-        self.demanda = demanda
-        self.institucion = institucion
-
-class DemandaVinculada:
-    def __init__(self, demanda_1: Demanda, demanda_2: Demanda):
-        self.demanda_1 = demanda_1
-        self.demanda_2 = demanda_2
-
-class Legajo:
-    def __init__(self, info_legajo: str, nnya: NNyA):
-        self.info_legajo = info_legajo
-        self.nnya = nnya
-
-class LegajoAsignado:
-    def __init__(self, legajo: Legajo, user: User, esta_activo: bool = True, recibido: bool = False):
-        self.legajo = legajo
-        self.user = user
-        self.esta_activo = esta_activo
-        self.recibido = recibido
-
-class IndicadoresValoracion:
-    def __init__(self, nombre: str, descripcion: str = None, peso: int = 0):
-        self.nombre = nombre
-        self.descripcion = descripcion
-        self.peso = peso
-
-class Evaluaciones:
-    def __init__(self, demanda: Demanda, indicador: IndicadoresValoracion, si_no: bool = False):
-        self.demanda = demanda
-        self.indicador = indicador
-        self.si_no = si_no
-
-class Vinculo:
-    def __init__(self, nombre: str):
-        self.nombre = nombre
-
-class VinculoPersonaPersona:
-    def __init__(self, conviven: bool, vinculo: Vinculo, persona_1: Persona, persona_2: Persona):
-        self.conviven = conviven
-        self.vinculo = vinculo
-        self.persona_1 = persona_1
-        self.persona_2 = persona_2
-
-class VinculoPersonaNNyA:
-    def __init__(self, conviven: bool, autordv: bool, garantiza_proteccion: bool, vinculo: Vinculo, nnya: NNyA, persona: Persona):
-        self.conviven = conviven
-        self.autordv = autordv
-        self.garantiza_proteccion = garantiza_proteccion
-        self.vinculo = vinculo
-        self.nnya = nnya
-        self.persona = persona
-
-class Score:
-    def __init__(self, demanda: Demanda, nnya: NNyA, score: int, score_vulneracion: int, score_evaluacion: int, score_condiciones_vulnerabilidad: int, score_motivo_intervencion: int):
-        self.demanda = demanda
-        self.nnya = nnya
-        self.score = score
-        self.score_vulneracion = score_vulneracion
-        self.score_evaluacion = score_evaluacion
-        self.score_condiciones_vulnerabilidad = score_condiciones_vulnerabilidad
-        self.score_motivo_intervencion = score_motivo_intervencion
-
-class CondicionesVulnerabilidad:
-    def __init__(self, nombre: str, descripcion: str, peso: int):
-        self.nombre = nombre
-        self.descripcion = descripcion
-        self.peso = peso
-
-class NNyACondicionesVulnerabilidad:
-    def __init__(self, nnya: NNyA, condiciones_vulnerabilidad: CondicionesVulnerabilidad, si_no: bool):
-        self.nnya = nnya
-        self.condiciones_vulnerabilidad = condiciones_vulnerabilidad
-        self.si_no = si_no
-
-class MotivoIntervencion:
-    def __init__(self, nombre: str, descripcion: str, peso: int):
-        self.nombre = nombre
-        self.descripcion = descripcion
-        self.peso = peso
-
-class NNyAMotivoIntervencion:
-    def __init__(self, nnya: NNyA, motivo_intervencion: MotivoIntervencion):
-        self.nnya = nnya
-        self.motivo_intervencion = motivo_intervencion
--- runna/core/use_cases.py
+++ runna/core/use_cases.py
@@ -16,210 +16,58 @@ class ProductUseCase:
 '''
 
 from core.entities import (
-    User, ActividadTipo, CategoriaMotivo, CategoriaSubmotivo, Decision, DemandaAsignado, DemandaPersona, GravedadVulneracion
-    , InstitucionActividad, InstitucionEducativa, InstitucionRespuesta, InstitucionSanitaria, NNyA, NNyAEducacion, Provincia
-    , Departamento, Localidad, Barrio, CPC, Localizacion, InstitucionUsuarioLinea, UrgenciaVulneracion, VinculoUsuarioLinea
-    , Cargo, Responsable, UsuarioLinea, Demanda, PrecalificacionDemanda, Persona, Vulneracion
-    , Actividad, Respuesta, DemandaVinculada, Legajo, LegajoAsignado, IndicadoresValoracion, Evaluaciones, Vinculo
-    , VinculoPersonaNNyA, VinculoPersonaNNyA, Score, CondicionesVulnerabilidad, NNyACondicionesVulnerabilidad
-    , MotivoIntervencion, NNyAMotivoIntervencion, VinculoPersonaPersona
+    Provincia, Departamento, Localidad, Barrio, CPC, Localizacion
     )
 
-class UserUseCase:
-    def create_custom_user(self, username, email, fecha_nacimiento=None, genero=None, elefono=None):
-        return User(username, email, fecha_nacimiento, genero, elefono)
-
 class TProvinciaUseCase:
-    def create_provincia(self, nombre):
-        return Provincia(nombre)
+    def list_provincias(self, queryset):
+        return [Provincia(provincia.id, provincia.nombre) for provincia in queryset]
+    
+    def get_provincia(self, provincia):
+        return Provincia(provincia.id, provincia.nombre)
+
 
 class TDepartamentoUseCase:
-    def create_departamento(self, nombre):
-        return Departamento(nombre)
+    def list_departamentos(self, queryset):
+        return [Departamento(departamento.id, departamento.nombre, departamento.provincia) for departamento in queryset]
+    
+    def get_departamento(self, departamento):
+        return Departamento(departamento.id, departamento.nombre, departamento.provincia)
 
 class TLocalidadUseCase:
-    def create_localidad(self, nombre, provincia, departamento):
-        return Localidad(nombre, provincia, departamento)
+    def list_localidades(self, queryset):
+        return [Localidad(localidad.id, localidad.nombre, localidad.departamento) for localidad in queryset]
+    
+    def get_localidad(self, localidad):
+        return Localidad(localidad.id, localidad.nombre, localidad.departamento)
 
 class TBarrioUseCase:
-    def create_barrio(self, nombre, localidad):
-        return Barrio(nombre, localidad)
+    def list_barrios(self, queryset):
+        return [Barrio(barrio.id, barrio.nombre, barrio.localidad) for barrio in queryset]
+    
+    def get_barrio(self, barrio):
+        return Barrio(barrio.id, barrio.nombre, barrio.localidad)
 
 class TCPCUseCase:
-    def create_cpc(self, nombre):
-        return CPC(nombre)
+    def list_cpcs(self, queryset):
+        return [CPC(cpc.id, cpc.nombre, cpc.localidad) for cpc in queryset]
+    
+    def get_cpc(self, cpc):
+        return CPC(cpc.id, cpc.nombre, cpc.localidad)
 
 class TLocalizacionUseCase:
-    def create_localizacion(self, calle, ipo_calle, barrio, localidad, cpc=None, piso_depto=None, lote=None, mza=None, casa_nro=None, referencia_geo=None):
-        return Localizacion(calle, ipo_calle, barrio, localidad, cpc, piso_depto, lote, mza, casa_nro, referencia_geo)
-
-class TInstitucionUsuarioLineaUseCase:
-    def create_institucion_usuario_linea(self, nombre, contacto):
-        return InstitucionUsuarioLinea(nombre, contacto)
-
-class TVinculoUsuarioLineaUseCase:
-    def create_vinculo_usuario_linea(self, nombre):
-        return VinculoUsuarioLinea(nombre)
-
-class TCargoUseCase:
-    def create_cargo(self, nombre):
-        return Cargo(nombre)
-
-class TResponsableUseCase:
-    def create_responsable(self, nombre, apellido, cargo=None):
-        return Responsable(nombre, apellido, cargo)
-
-class TUsuarioLineaUseCase:
-    def create_usuario_linea(self, nombre, apellido, genero, vinculo_usuario_linea, institucion_usuario_linea, responsable=None, fecha_nacimiento=None, elefono=None):
-        return UsuarioLinea(nombre, apellido, genero, vinculo_usuario_linea, institucion_usuario_linea, responsable, fecha_nacimiento, elefono)
-
-class TDemandaUseCase:
-    def create_demanda(self, fecha_ingreso, hora_ingreso, origen, localizacion, usuario_linea, nro_notificacion_102=None, nro_sac=None, nro_suac=None, nro_historia_clinica=None, nro_oficio_web=None, descripcion=None):
-        return Demanda(fecha_ingreso, hora_ingreso, origen, localizacion, usuario_linea, nro_notificacion_102, nro_sac, nro_suac, nro_historia_clinica, nro_oficio_web, descripcion)
+    def create_localizacion(self, calle, tipo_calle, localidad, barrio=None, cpc=None, piso_depto=None, lote=None, mza=None, casa_nro=None, referencia_geo=None):
+        return Localizacion(None, calle, tipo_calle, localidad, barrio, cpc, piso_depto, lote, mza, casa_nro, referencia_geo)
     
-    def update_score(self, demanda, new_score):
-        """Updates the score of the Demanda."""
-        demanda.score = new_score
-        return demanda
-
-class TPrecalificacionDemandaUseCase:
-    def create_precalificacion_demanda(self, fecha, hora, descripcion, estado_demanda, demanda):
-        return PrecalificacionDemanda(fecha, hora, descripcion, estado_demanda, demanda)
-
-class TPersonaUseCase:
-    def create_persona(self, nombre, apellido, fecha_nacimiento, situacion_dni, genero, edad_aproximada=None, dni=None, boton_antipanico=False, observaciones=None, adulto=False, nnya=False):
-        return Persona(nombre, apellido, fecha_nacimiento, situacion_dni, genero, edad_aproximada, dni, boton_antipanico, observaciones, adulto, nnya)
+    def list_localizaciones(self, queryset):
+        return [Localizacion(localizacion.id, localizacion.calle, localizacion.tipo_calle, localizacion.localidad, localizacion.barrio, localizacion.cpc, localizacion.piso_depto, localizacion.lote, localizacion.mza, localizacion.casa_nro, localizacion.referencia_geo) for localizacion in queryset]
     
-    def update_persona(self, persona, observaciones):
-        """Updates the observaciones of the persona."""
-        persona.observaciones = observaciones
-        return persona
+    def get_localizacion(self, localizacion):
+        return Localizacion(localizacion.id, localizacion.calle, localizacion.tipo_calle, localizacion.localidad, localizacion.barrio, localizacion.cpc, localizacion.piso_depto, localizacion.lote, localizacion.mza, localizacion.casa_nro, localizacion.referencia_geo)
     
-class TDemandaPersonaUseCase:
-    def create_demanda_persona(self, demanda, persona, conviviente=False, supuesto_autordv=False, supuesto_autordv_principal=False, nnya=False, nnya_principal=False):
-        return DemandaPersona(demanda, persona, conviviente, supuesto_autordv, supuesto_autordv_principal, nnya, nnya_principal)
-
-class TInstitucionEducativaUseCase:
-    def create_institucion_educativa(self, nombre, mail=None, elefono=None):
-        return InstitucionEducativa(nombre, mail, elefono)
-
-class TNNyAEducacionUseCase:
-    def create_nnya_educacion(self, curso, nivel, urno, comentarios=None, institucion_educativa=None):
-        return NNyAEducacion(curso, nivel, urno, comentarios, institucion_educativa)
-
-class TInstitucionSanitariaUseCase:
-    def create_institucion_sanitaria(self, nombre, mail=None, elefono=None):
-        return InstitucionSanitaria(nombre, mail, elefono)
-
-class TNNyAUseCase:
-    def create_nnya(self, persona, educacion, institucion_sanitaria):
-        return NNyA(persona, educacion, institucion_sanitaria)
-
-class TCategoriaMotivoUseCase:
-    def create_categoria_motivo(self, nombre, descripcion=None, peso=0):
-        return CategoriaMotivo(nombre, descripcion, peso)
-
-class TCategoriaSubmotivoUseCase:
-    def create_categoria_submotivo(self, nombre, descripcion=None, peso=0, motivo=None):
-        return CategoriaSubmotivo(nombre, descripcion, peso, motivo)
-
-class TGravedadVulneracionUseCase:
-    def create_gravedad_vulneracion(self, nombre, descripcion=None, peso=0):
-        return GravedadVulneracion(nombre, descripcion, peso)
-
-class TUrgenciaVulneracionUseCase:
-    def create_urgencia_vulneracion(self, nombre, descripcion=None, peso=0):
-        return UrgenciaVulneracion(nombre, descripcion, peso)
-
-class TVulneracionUseCase:
-    def create_vulneracion(self, principal_demanda, ranscurre_actualidad, sumatoria_de_pesos, demanda, nnya, autor_dv=None, categoria_motivo=None, categoria_submotivo=None, gravedad_vulneracion=None, urgencia_vulneracion=None):
-        return Vulneracion(principal_demanda, ranscurre_actualidad, sumatoria_de_pesos, demanda, nnya, autor_dv, categoria_motivo, categoria_submotivo, gravedad_vulneracion, urgencia_vulneracion)
-
-    def calculate_vulneracion_score(self, vulneracion):
-        """Calculates and returns the score of a Vulneracion based on its properties."""
-        return vulneracion.sumatoria_pesos
-
-class TDecisionUseCase:
-    def create_decision(self, fecha, hora, justificacion, decision, demanda):
-        return Decision(fecha, hora, justificacion, decision, demanda)
-    
-    def make_decision(self, fecha, hora, decision, justificacion, demanda):
-        """Creates a new Decision entity."""
-        return Decision(fecha, hora, decision, justificacion, demanda)
-
-class TDemandaAsignadoUseCase:
-    def create_demanda_asignado(self, demanda, user, esta_activo=True, recibido=False, comentarios=None):
-        return DemandaAsignado(demanda, user, esta_activo, recibido, comentarios)
-
-class TActividadTipoUseCase:
-    def create_actividad_tipo(self, nombre):
-        return ActividadTipo(nombre)
-
-class TInstitucionActividadUseCase:
-    def create_institucion_actividad(self, nombre):
-        return InstitucionActividad(nombre)
-
-class TInstitucionRespuestaUseCase:
-    def create_institucion_respuesta(self, nombre):
-        return InstitucionRespuesta(nombre)
-
-
-class TActividadUseCase:
-    def create_actividad(self, fecha, hora, descripcion, demanda, ipo=None, institucion=None):
-        return Actividad(fecha, hora, descripcion, demanda, ipo, institucion)
-
-class TRespuestaUseCase:
-    def create_respuesta(self, fecha, hora, mail, mensaje, demanda, institucion=None):
-        return Respuesta(fecha, hora, mail, mensaje, demanda, institucion)
-
-class TDemandaVinculadaUseCase:
-    def create_demanda_vinculada(self, demanda_1, demanda_2):
-        return DemandaVinculada(demanda_1, demanda_2)
-
-class TLegajoUseCase:
-    def create_legajo(self, info_legajo, nnya):
-        return Legajo(info_legajo, nnya)
-
-class TLegajoAsignadoUseCase:
-    def create_legajo_asignado(self, legajo, user, esta_activo=True, recibido=False):
-        return LegajoAsignado(legajo, user, esta_activo, recibido)
-
-class TIndicadoresValoracionUseCase:
-    def create_indicadores_valoracion(self, nombre, descripcion=None, peso=0):
-        return IndicadoresValoracion(nombre, descripcion, peso)
-
-class TEvaluacionesUseCase:
-    def create_evaluaciones(self, demanda, indicador, si_no=False):
-        return Evaluaciones(demanda, indicador, si_no)
-
-class TVinculoUseCase:
-    def create_vinculo(self, nombre):
-        return Vinculo(nombre)
-
-class TVinculoPersonaPersonaUseCase:
-    def create_vinculo_persona_persona(self, conviven, vinculo, persona_1, persona_2):
-        return VinculoPersonaPersona(conviven, vinculo, persona_1, persona_2)
-
-class TVinculoPersonaNNyAUseCase:
-    def create_vinculo_persona_nnya(self, conviven, autordv, garantiza_proteccion, vinculo, nnya, persona):
-        return VinculoPersonaNNyA(conviven, autordv, garantiza_proteccion, vinculo, nnya, persona)
-
-class TScoreUseCase:
-    def create_score(self, demanda, nnya, score, score_vulneracion, score_evaluacion, score_condiciones_vulnerabilidad, score_motivo_intervencion):
-        return Score(demanda, nnya, score, score_vulneracion, score_evaluacion, score_condiciones_vulnerabilidad, score_motivo_intervencion)
-
-class TCondicionesVulnerabilidadUseCase:
-    def create_condiciones_vulnerabilidad(self, nombre, descripcion, peso):
-        return CondicionesVulnerabilidad(nombre, descripcion, peso)
-
-class TNNyACondicionesVulnerabilidadUseCase:
-    def create_nnya_condiciones_vulnerabilidad(self, nnya, condiciones_vulnerabilidad, si_no):
-        return NNyACondicionesVulnerabilidad(nnya, condiciones_vulnerabilidad, si_no)
-
-class TMotivoIntervencionUseCase:
-    def create_motivo_intervencion(self, nombre, descripcion, peso):
-        return MotivoIntervencion(nombre, descripcion, peso)
-
-class TNNyAMotivoIntervencionUseCase:
-    def create_nnya_motivo_intervencion(self, nnya, motivo_intervencion):
-        return NNyAMotivoIntervencion(nnya, motivo_intervencion)
+    def update_localizacion(self, localizacion, **kwargs):
+        for key, value in kwargs.items():
+            if hasattr(localizacion, key):
+                setattr(localizacion, key, value)
+        
+        return localizacion
