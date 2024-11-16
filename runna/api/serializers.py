4d9b5a30ea07c32d6340898f3cd1775414d3f9bc
facuolivamar <facundoolivam@gmail.com>
2024-11-16 17:35:26 -0300

    Serializers: PERMISSIONS FILTERING

--- runna/api/serializers.py
+++ runna/api/serializers.py
@@ -11,6 +11,43 @@ from infrastructure.models import (
     TProvincia, TDepartamento, TLocalidad, TBarrio, TCPC, TLocalizacion
 )
 
+from infrastructure.models import CustomUser
+from django.contrib.auth.models import Group, Permission
+
+class PermissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Permission
+        fields = ['id', 'name', 'codename']
+
+class GroupSerializer(serializers.ModelSerializer):
+    # permissions = PermissionSerializer(many=True, source='permissions.all')
+
+    class Meta:
+        model = Group
+        fields = ['id', 'name'] #'permissions']
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    groups = GroupSerializer(many=True)  # Serialize groups with permissions
+    # user_permissions = PermissionSerializer(many=True)
+    all_permissions = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CustomUser
+        fields = [
+            'id', 'username', 'first_name', 'last_name', 'email',
+            'fecha_nacimiento', 'genero', 'telefono', 'is_staff',
+            'is_active', 'is_superuser', 'groups', 'user_permissions',
+            'all_permissions'
+        ]
+
+    def get_all_permissions(self, obj):
+        """Return both user-specific and group-inherited permissions."""
+        # Collect both direct and group permissions
+        permissions = obj.user_permissions.all() | Permission.objects.filter(group__user=obj)
+        return PermissionSerializer(permissions, many=True).data
+
+
 class TProvinciaSerializer(serializers.ModelSerializer):
     class Meta:
         model = TProvincia
