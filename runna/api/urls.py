3876b6c1ade335b2d5014049dde23bd48d5b53cb
facuolivamar <facundoolivam@gmail.com>
2024-11-16 17:35:35 -0300

    CustomUser endpoint

--- runna/api/urls.py
+++ runna/api/urls.py
@@ -1,5 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from api.views import TProvinciaViewSet, TDepartamentoViewSet, TLocalidadViewSet, TBarrioViewSet, TCPCViewSet, TLocalizacionViewSet
+from api.views import CustomUserViewSet
 
 router = DefaultRouter()
 router.register(r'provincia', TProvinciaViewSet, basename='provincia')
@@ -8,5 +9,6 @@ router.register(r'localidad', TLocalidadViewSet, basename='localidad')
 router.register(r'barrio', TBarrioViewSet, basename='barrio')
 router.register(r'cpc', TCPCViewSet, basename='cpc')
 router.register(r'localizacion', TLocalizacionViewSet, basename='localizacion')
+router.register(r'user', CustomUserViewSet, basename='user')
 
 urlpatterns = router.urls
