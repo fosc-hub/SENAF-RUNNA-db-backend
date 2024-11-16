507eca0a1998be446492ca2dd30dbfb2d00891c9
facuolivamar <facundoolivam@gmail.com>
2024-11-16 17:35:01 -0300

    CustomUser filters

--- runna/infrastructure/filters.py
+++ runna/infrastructure/filters.py
@@ -1,5 +1,19 @@
 from django_filters import rest_framework as filters
 from infrastructure.models import TProvincia, TDepartamento, TLocalidad, TBarrio, TCPC, TLocalizacion
+from infrastructure.models import CustomUser
+from django.contrib.auth.models import Group
+
+class CustomUserFilter(filters.FilterSet):
+    username = filters.CharFilter(lookup_expr='icontains')  # Partial match for username
+    email = filters.CharFilter(lookup_expr='icontains')  # Partial match for email
+    is_staff = filters.BooleanFilter()  # Filter by staff status
+    is_active = filters.BooleanFilter()  # Filter by active status
+    is_superuser = filters.BooleanFilter()
+    groups = filters.ModelMultipleChoiceFilter(queryset=Group.objects.all())  # Filter by group
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'is_staff', 'is_active', 'groups']
 
 class TProvinciaFilter(filters.FilterSet):
     nombre = filters.CharFilter(lookup_expr='icontains')
