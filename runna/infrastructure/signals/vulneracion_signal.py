7d56e4f755d0b4af099283b84e0dd416902d8fac
facuolivamar <facundoolivam@gmail.com>
2024-11-23 13:07:59 -0300

    Vulneracion signals (triggers) made reusable-extensible

--- runna/infrastructure/signals/vulneracion_signal.py
+++ runna/infrastructure/signals/vulneracion_signal.py
@@ -1,42 +1,25 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save, post_delete, pre_delete
 from django.dispatch import receiver
 from infrastructure.models import TVulneracion, TVulneracionHistory
 from runna.middleware import get_current_authenticated_user
+
+def logs(HistoryModelName, action, instance):
+    HistoryModelName.objects.create(
+        vulneracion_parent=instance,
+        action=action,
+        user=get_current_authenticated_user(),
+        **{field.name: getattr(instance, field.name, None) for field in instance._meta.fields if field.name != 'id'}
+    )
 
 
 @receiver(post_save, sender=TVulneracion)
 def log_vulneracion_save(sender, instance, created, **kwargs):
     action = 'CREATE' if created else 'UPDATE'
-    TVulneracionHistory.objects.create(
-        vulneracion_parent=instance,
-        action=action,
-        user=get_current_authenticated_user(),  # Replace with logic to get the current user
-        principal_demanda=instance.principal_demanda,
-        transcurre_actualidad=instance.transcurre_actualidad,
-        sumatoria_de_pesos=instance.sumatoria_de_pesos,
-        demanda=instance.demanda,
-        nnya=instance.nnya,
-        autor_dv=instance.autor_dv,
-        categoria_motivo=instance.categoria_motivo,
-        categoria_submotivo=instance.categoria_submotivo,
-        gravedad_vulneracion=instance.gravedad_vulneracion,
-        urgencia_vulneracion=instance.urgencia_vulneracion,
-    )
+    logs(TVulneracionHistory, action, instance)
+
 
 @receiver(post_delete, sender=TVulneracion)
 def log_vulneracion_delete(sender, instance, **kwargs):
-    TVulneracionHistory.objects.create(
-        vulneracion_parent=instance,
-        action='DELETE',
-        user=get_current_authenticated_user(),  # Replace with logic to get the current user
-        principal_demanda=instance.principal_demanda,
-        transcurre_actualidad=instance.transcurre_actualidad,
-        sumatoria_de_pesos=instance.sumatoria_de_pesos,
-        demanda=instance.demanda,
-        nnya=instance.nnya,
-        autor_dv=instance.autor_dv,
-        categoria_motivo=instance.categoria_motivo,
-        categoria_submotivo=instance.categoria_submotivo,
-        gravedad_vulneracion=instance.gravedad_vulneracion,
-        urgencia_vulneracion=instance.urgencia_vulneracion,
-    )
+    action='DELETE'
+    logs(TVulneracionHistory, action, instance)
+
