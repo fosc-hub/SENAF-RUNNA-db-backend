f3d8a4fa0f479512e2bac417bee751e3f70e3b64
SantiagoCarranza <santiagocarranzazinny@gmail.com>
2024-11-28 14:54:57 -0300

    ffixing

--- runna/runna/wsgi.py
+++ runna/runna/wsgi.py
@@ -12,8 +12,9 @@ from django.core.wsgi import get_wsgi_application
 import sys
 print("Python path:", sys.path)
 print("DJANGO_SETTINGS_MODULE:", os.environ.get('DJANGO_SETTINGS_MODULE'))
+sys.path.append('/var/task/runna')
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'runna.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'runna.runna.settings')
 
 application = get_wsgi_application()
 
