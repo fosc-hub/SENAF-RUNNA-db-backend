d1ab58783ec6a7b2450a2778db7a14451b714961
facuolivamar <facundoolivam@gmail.com>
2025-02-25 21:16:04 -0300

    CAMBIO: Eliminar autenticación de sesión y básica en la configuración de REST framework

--- runna/runna/settings.py
+++ runna/runna/settings.py
@@ -289,8 +289,6 @@ AUTH_USER_MODEL = 'customAuth.CustomUser'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
@@ -303,9 +301,6 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 5,  # Adjust as needed
 }
-
-SESSION_ENGINE = 'django.contrib.sessions.backends.db'
-
 
 SPECTACULAR_SETTINGS = {
     'TITLE': 'RUNNA API',
