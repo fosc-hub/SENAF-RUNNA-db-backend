c38618352c79fe86f93c7e283b06b91b74d44cbf
facuolivamar <facundoolivam@gmail.com>
2025-02-27 05:17:21 -0300

    FEAT: Simplificar la vista de sugerencia de decisión eliminando el parámetro nnya_id y ajustando la lógica de puntuación

--- runna/api/views/SuggestDecision.py
+++ runna/api/views/SuggestDecision.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from drf_spectacular.utils import extend_schema
 from core.use_cases import TDecisionUseCase
-from infrastructure.models import TNNyAScore, TDemandaScore
+from infrastructure.models import TNNyAScore, TDemandaScore, TDemandaPersona
 from api.serializers import TPersonaSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 class SuggestDecisionView(APIView):
@@ -17,16 +17,34 @@ class SuggestDecisionView(APIView):
         },
         description="Suggest a decision based on NNyA and related Demanda scores."
     )
-    def get(self, request, nnya_id, demanda_id):
+    def get(self, request, demanda_id):
         try:
-            # Fetch the NNyA object
-            nnya_score = TNNyAScore.objects.get(nnya=nnya_id)
             demanda_score = TDemandaScore.objects.get(demanda=demanda_id)
-        except TNNyAScore.DoesNotExist:
-            return Response(
-                {"error": "NNyA with the given ID does not exist."},
-                status=status.HTTP_404_NOT_FOUND
-            )
+            nnyas = TDemandaPersona.objects.filter(demanda=demanda_id, persona__nnya=True)
+            print(f"NNyAs: {nnyas}")
+            if not nnyas.exists():
+                return Response(
+                    {"error": "No NNyA associated with the given Demanda."},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+
+            # Find the NNyA with the highest score
+            highest_score_nnya = None
+            highest_score = -1
+            for demanda_persona in nnyas:
+                try:
+                    nnya_score = TNNyAScore.objects.get(nnya=demanda_persona.persona)
+                    if nnya_score.score > highest_score:
+                        highest_score = nnya_score.score
+                        highest_score_nnya = nnya_score
+                except TNNyAScore.DoesNotExist:
+                    continue
+
+            if highest_score_nnya is None:
+                return Response(
+                    {"error": "No scores found for NNyAs associated with the given Demanda."},
+                    status=status.HTTP_404_NOT_FOUND
+                )
         except TDemandaScore.DoesNotExist:
             return Response(
                 {"error": "Demanda with the given ID does not exist."},
@@ -35,6 +53,8 @@ class SuggestDecisionView(APIView):
 
         # Use case logic
         decision_use_case = TDecisionUseCase()
-        suggestion = decision_use_case.suggest_decision(nnya_score, demanda_score)
+        suggestion = decision_use_case.suggest_decision(highest_score_nnya, demanda_score)
+        suggestion["NNyA"] = TPersonaSerializer(highest_score_nnya.nnya).data
+        
 
         return Response(suggestion, status=status.HTTP_200_OK)
